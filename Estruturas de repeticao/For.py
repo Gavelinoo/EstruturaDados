3c62d317734facea08bbfde18e5c0e0841b9a481
Gleison Carneiro Avelino <gavelino@sarapui>
2025-09-25 21:31:45 -0300

    mudancas

---
+++ Estruturas de repeticao/For.py
@@ -0,0 +1,16 @@
+# # iterando sobre range
+#
+# for i in range(5):
+#     print(i)
+#
+# # iterando sobre string
+#
+#
+#
+# for i in "Python":
+#     print(i)
+
+#iterando com enumrate
+frutas = ["maçã","banana","laranja"]
+for indice, fruta in enumerate(frutas):
+    print(f"{indice}: {fruta}")
