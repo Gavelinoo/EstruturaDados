5bf63e5b1dcc80d9c443ac181b70304866f1e38e
Gleison Carneiro Avelino <gavelino@sarapui>
2025-09-29 20:23:02 -0300

    Loop Continue

--- Estruturas de repeticao/For.py
+++ Estruturas de repeticao/For.py
@@ -10,7 +10,15 @@
 # for i in "Python":
 #     print(i)
 
-#iterando com enumrate
-frutas = ["maçã","banana","laranja"]
-for indice, fruta in enumerate(frutas):
-    print(f"{indice}: {fruta}")
+# #iterando com enumrate
+# frutas = ["maçã","banana","laranja"]
+# for indice, fruta in enumerate(frutas):
+#     print(f"{indice}: {fruta}")
+
+
+# Loop com continue
+for num in range(10):
+    if num % 2 != 0:
+        continue
+    print(num)
+
