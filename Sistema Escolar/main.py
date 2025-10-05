c7c0da4fb2939dc203da838b79a6e6add6c6e65f
Gaavelino <gleisonavelino@hotmail.com>
2025-10-05 11:03:44 -0300

    exercicio super

--- Sistema Escolar/main.py
+++ Sistema Escolar/main.py
@@ -1,16 +1,18 @@
 from Pessoa import Pessoa
 from Professor import Professor
 from Aluno import Aluno
+from Funcionario import Funcionario
 
 def main():
     Jorge = Pessoa("Jorge", "35", "12345678910", "Sarapui 8", "21252781")
     Bruno = Professor("Bruno", "25", "123456788", "Rua Mexico", "2154655622", "Estrutura de dados")
     Israel = Aluno("Israel", "25", "3234123212", "Quartel", "23423234324", "123")
+    Manoel = Funcionario("Manoel", "56", "41234234233","Rua do catete", "252781286","Zelador")
 
 
     Jorge.mostrar_detalhes()
     Bruno.mostrar_detalhes()
-    Israel.mostrar_detalhes()
+    Manoel.mostrar_detalhes()
 
 
 if __name__ == "__main__":
